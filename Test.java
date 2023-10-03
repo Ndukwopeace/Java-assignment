e930685f050490b5fe4bcd1807494987fd0124e4
MacBook Pro <macbookpro@Kalu-Peace.local>
2023-10-03 13:30:26 +0100

    first java assignment

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+   public static void main(String[] args){
+    System.out.println("My name is Kalu Ndukwo");
+    System.out.println("I am 26 years Old");
+    System.out.println("My Hometown is Douala");
+
+
+   } 
+}
